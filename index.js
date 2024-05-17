317ca0895656e3b8515342e773e1ebdec7ae061b
Lenka Zufala <m-lenkazufala@seznam.cz>
2024-05-17 08:29:01 +0200

    cg

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const questionEl = document.querySelector (".question")
+const question = "Who was the first president of USA?"
+
+questionEl.textContent = (question)
+
+const userAnswer =(prompt(question))
+const answerEL= document.querySelector (".answer__text")
+const elAnswer = document.querySelector (".answer")
+
+answerEL.textContent = userAnswer
+
+if (userAnswer === "Washington"){
+    elAnswer.classList.add ("answer--correct")
+} else {
+    elAnswer.classList.add ("answer--wrong")
+}
+
+
+
+
 
